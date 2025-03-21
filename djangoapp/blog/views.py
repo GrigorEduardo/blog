dfc1177dc5aded03f6151aa71e7545440fbbd14c
GrigorEduardo <gromioresponde27@gmail.com>
2025-03-20 21:47:44 -0300

    layout templates

--- djangoapp/blog/views.py
+++ djangoapp/blog/views.py
@@ -5,3 +5,14 @@ def index(request):
     return render(
         request, 'blog/pages/index.html'
     )
+
+
+def page(request):
+    return render(
+        request, 'blog/pages/page.html'
+    )
+
+def post(request):
+    return render(
+        request, 'blog/pages/post.html'
+    )
